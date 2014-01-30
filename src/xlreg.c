af8a0f9b39b96366689e91ec669adb3e5d4c7575
Jim Dixon <jddixon@gmail.com>
2014-01-29 16:25:02 -0800

    sketch of clientEncryptHello

--- src/xlreg.c
+++ src/xlreg.c
@@ -1,2 +1,27 @@
 ## xlreg/src/xlreg.c
 
+#include <openssl/rsa.h>
+
+#define MSG_LEN 60
+
+/**
+ * Caller allocates a buffer cipherTextof MSG_LEN bytes and passes a pointer
+ * to it (*cipherText).  Caller also passes the proposed version number.
+ *
+ * Should return -1 on failure, or more informativ etive error code.
+ */
+
+int clientEncryptHello(pubKey *RSA, version uint32_t, cipherText *uint8_t) {
+
+    msg uint8_t[MSG_LEN]
+
+    // Fill the first 56 bytes of the buffer  with random numbers.
+    // XXX STUB
+
+    // The uint32_t version number is added to the msg.  
+    // XXX STUB
+
+    // RSA-OAEP the message
+    return RSA_public_encrypt(MSG_LEN, &msg, cipherText, RSA, 
+        RSA_PKCS1_OAEP_PADDING)
+}
--- src/xlreg.h
+++ src/xlreg.h
@@ -3,5 +3,7 @@
 #ifndef XLREG_H
 #define XLREG_H
 
+#include <stdio.h>          // for FILE
+#include <stdint.h>
 
 #endif /* XLREG_H */
